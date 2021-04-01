d985a99d6b079bbe80da6b0b473f6f3afe287f8b
Neil <zooming.in@hotmail.com>
2021-04-01 23:14:30 +1100

    updates

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue <= 400) {result = "This one is on me!"}
+  else if (someValue > 2500) {result = "No can do."}
+  else if (someValue > 2000) {result = "I will gladly take your thirty bucks."}
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  let returnCity
+  someCity === "NYC" ? (returnCity = "Ok, sounds good.") : (returnCity = "No go.")
+  return returnCity
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  let response
+  switch (tipAmount) {
+    case 'generous': response = 'Thank you so much.'
+    break;
+    case 'not as generous': response = 'Thank you.'
+    break;
+    default: response = 'Bye.'
+  }
+  return response
 }
